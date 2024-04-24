726fa36e0f0f7d0bb45f36ecac5efe58034a58a9
kataoka-tarasi <tky2302083@stu.o-hara.ac.jp>
2024-04-24 11:11:49 +0900

    SubjectListAction.java 三ノ輪

---
+++ src/scoremanager/main/SubjectListAction.java
@@ -0,0 +1,37 @@
+package scoremanager.main;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import javax.security.auth.Subject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Teacher;
+import dao.ClassNumDao;
+import tool.Action;
+
+public class SubjectListAction extends Action {
+	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+		HttpSession session = req.getSession();//セッション
+		Teacher teacher = (Teacher)session.getAttribute("user");
+		
+		ClassNumDao cNumDao = new ClassNumDao();// クラス番号Daoを初期化
+		SubjectDao sDao = new SubjectDao();// 
+		 LocalDate todaysDate = LocalDate.now();// LcalDateインスタンスを取得	
+		 int year = todaysDate.getYear();
+		
+		//ログインユーザーの学校コードをもとに、ユーザーが所属している学校の科目一覧用データを取得
+		List<Subject> list = sDao.filter(teacher.getSchool());
+		
+		
+		// リクエストにデータをセット
+		req.setAttribute("subject_set", list);
+		//JSPへフォワード
+		req.getRequestDispatcher("subject_list.jsp").forward(req, res);
+		
+		
+	}
+
+}
