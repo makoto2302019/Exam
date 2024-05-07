4348534c65738e9129b2bef6dd338608ea1db1ad
kataoka-tarasi <tky2302083@stu.o-hara.ac.jp>
2024-05-07 10:50:27 +0900

    aaa

---
+++ src/scoremanager/main/SubjectDeleteExecuteAction.java
@@ -0,0 +1,30 @@
+package scoremanager.main;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import bean.Subject;
+import dao.SubjectDao;
+import tool.Action;
+ 
+public class SubjectDeleteExecuteAction extends Action {
+ 
+    @Override
+    public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
+        // リクエストから削除する科目のコードを取得
+        String subCd = req.getParameter("cd");
+        String subName = req.getParameter("name");
+        
+        Subject p= new Subject();
+        p.setCd(subCd);
+        p.setName(subName);
+
+        // 科目Daoをインスタンス化
+        SubjectDao sDao = new SubjectDao();
+
+        // 科目Daoを使用して科目を削除
+        boolean success = sDao.delete(p);
+
+        req.getRequestDispatcher("subject_delete_done.jsp").forward(req, res);
+    }
+}
