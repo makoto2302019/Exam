31a2f4416859f1ba275fddac4e9ca718c910aacd
naru <tky2302130@stu.o-hara.ac.jp>
2024-05-15 12:22:57 +0900

    a

--- src/scoremanager/main/TestListAction.java
+++ src/scoremanager/main/TestListAction.java
@@ -13,55 +13,33 @@ import bean.Subject;
 import tool.Action;
 
 public class TestListAction extends Action {
-
     public void execute(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-
         setTestListSubject(req, res);
-
         setTestListStudent(req, res);
-
-
-        req.getRequestDispatcher("/path/to/test_list.jsp").forward(req, res);
-
+        req.getRequestDispatcher("test_list.jsp").forward(req, res);
     }
-
     // 科目リストをセットする
-
     public void setTestListSubject(HttpServletRequest req, HttpServletResponse res) {
-
-
         List<Subject> subjects = fetchSubjects();
         req.setAttribute("subjects", subjects);
-
     }
-
     // 学生リストをセットする
-
     public void setTestListStudent(HttpServletRequest req, HttpServletResponse res) {
 
 
         List<Student> students = fetchStudents();
         req.setAttribute("students", students);
-
     }
 
 
     private List<Subject> fetchSubjects() {
-
-
-
         return new ArrayList<>();
 
     }
 
     private List<Student> fetchStudents() {
-
-
         return new ArrayList<>();
 
     }
-
-
-
 
 }
