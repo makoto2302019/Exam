5024818746bba9f01e23a410fe734eb22886df91
kataoka-tarasi <tky2302083@stu.o-hara.ac.jp>
2024-05-17 12:24:53 +0900

    ああああああああああ

--- src/scoremanager/main/TestListAction.java
+++ src/scoremanager/main/TestListAction.java
@@ -1,10 +1,7 @@
-package scoremanager.main;
-
+package scoremanager.main; 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,87 +9,50 @@ import javax.servlet.http.HttpSession;
 
 import bean.Subject;
 import bean.Teacher;
-import bean.Test;
 import dao.ClassNumDao;
-import dao.StudentDao;
 import dao.SubjectDao;
-import dao.TestDao;
 import tool.Action;
-
 public class TestListAction extends Action {
-	
+
+	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
-		HttpSession session = req.getSession();//セッション
+		// ローカル変数の宣言１
+		HttpSession session = req.getSession();
 		Teacher teacher = (Teacher)session.getAttribute("user");
 		
-		TestDao tesDao = new TestDao();
-		SubjectDao suDao = new SubjectDao();
+		LocalDate todaysDate = LocalDate.now();	// LocalDateインスタンスを取得
+		int year = todaysDate.getYear();	// 現在の年を取得
+		// DAOをインスタンス化
 		ClassNumDao cNumDao = new ClassNumDao();
-		
-		
-		//ログインユーザーの学校コードをもとに、ユーザーが所属している学校の科目一覧用データを取得
-		List<Subject> list = suDao.filter(teacher.getSchool());
-		
-		// ログインユーザーの学校コードをもとにクラス番号の一覧を取得
-		List<String> list1 = cNumDao.filter(teacher.getSchool());
-		
-		List<Test> tests = null;
-		
-		
-		StudentDao sDao = new StudentDao();
-		Map<String, String> errors = new HashMap<>();
-		
-		
-		String entYearStr ="";//入力された入学年度
-		String classNum ="";//入力されたクラス番号
-		String subName = "";//入力された科目名
-		String Countstr ="";//入力された回数
-		int entYear = 0;//入学年度
-		int Count = 0;
-		LocalDate todaysDate = LocalDate.now();// LcalDateインスタンスを取得
-		int year = todaysDate.getYear();// 現在の年を取得
-		
-		entYearStr = req.getParameter("f1");
-		classNum = req.getParameter("f2");
-		subName = req.getParameter("f3");
-		Countstr = req.getParameter("f4");
-		
-		if (entYearStr !=null) {
-			//数値に変換
-			entYear = Integer.parseInt(entYearStr);
-			}
-		
-		if (Countstr !=null) {
-			//数値に変換
-			Count = Integer.parseInt(Countstr);
-			
-			
+		SubjectDao subDao = new SubjectDao();
+		// リクエストパラメーターの取得２
+		// なし 
+		// DBからデータの取得
+		// クラス番号の一覧をリストに取得
+		List<String> class_list = cNumDao.filter(teacher.getSchool());
+		// 科目コードと科目名をリストに取得
+		List<Subject> subject_list = subDao.filter(teacher.getSchool());
+		//ビジネスロジック 4
+		List<Integer> entYearSet = new ArrayList<>();	// リストを初期化
+		for (int i = year - 10; i < year + 1; i++) {	// 10年前から1年後まで年をリストに追加
+			entYearSet.add(i);
 		}
-		
-		Subject subject = new Subject();
-		subject.setName(subName);
-		
-		if(entYear == 0 || classNum == null || subName == null ){
-			errors.put("f1", "入学年度とクラスと科目を選択してください");
-			req.setAttribute("errors", errors);
-		} else {
-			
-			tests  = tesDao.filter(entYear,classNum,subject,Count,teacher.getSchool());
+		List<Integer> tesNumSet = new ArrayList<>();	// リストを初期化
+		for (int i = 1; i < 3; i++) {					// 1～2回のテストの回数をリストに追加
+			tesNumSet.add(i);
 		}
-		// リストを初期化
-				List<Integer> entYearSet = new ArrayList<>();
-				// 10年前から1年後まで年をリストに追加
-				for (int i = year -10; i < year + 1; i++) {
-					entYearSet.add(i);
-				}
-		
-		//リクエストにデータをセット
-		req.setAttribute("ent_year_set", entYearSet);
-		req.setAttribute("subject_list", list);
-		req.setAttribute("class_num_set", list1);
-		req.setAttribute("test_set", tests);
-		//JSPへフォワード
-		req.getRequestDispatcher("test_list.jsp").forward(req, res);
+ 
+ 
+		//DBへデータ保存 5
+		//なし
+		//レスポンス値をセット 6
+		req.setAttribute("ent_year_set", entYearSet);		// リクエストに入学年度をセット
+		req.setAttribute("class_num_set", class_list);		// リクエストにクラス番号をセット
+		req.setAttribute("subject_list", subject_list);		// リクエストに科目情報をセット
+		req.setAttribute("tes_num_set", tesNumSet);				// リクエストにテストの回数をセット
+		//JSPへフォワード 7
+		req.getRequestDispatcher("test_list.jsp").forward(req, res); // 学生一覧まで画面遷移
+
 	}
 
 }
